3dd713d95303d2fdcca3d6d019a7dc01dc28b150
VENTURA Simone <simone.ventura@soprasteria.com>
2018-10-16 17:33:57 +0200

    Countdown finito e corretto bootstrap slider nella versione mobile

--- script.js
+++ script.js
@@ -9,5 +9,20 @@ $(".owl-carousel").owlCarousel(settings);
 window.scrollTo(0,0);
 
 $('.count-box').countdown('2019/01/01', function(event) {
-  $(".count-date").text(event.strftime('%D Giorni %H:%M:%S a capodanno'));
-});
+  $(".count-box h1").text(event.strftime('%D Giorni %H:%M:%S'));
+
+}).on('update.countdown',function(){
+  	$(".count-box").css('box-shadow','4px 3px 77px 9px rgba(129,212,250,1)');
+  	setTimeout(()=>{
+  		$(".count-box").css('box-shadow','none');
+  	},900);
+  })
+
+  /*
+  $(".circle").css('box-shadow','4px 3px 77px 9px rgba(129,212,250,1)');
+  setTimeout(function(){
+  	$(".circle").css('box-shadow','none');
+  },1000);*/
+
+
+
